b30fae2ecd559cea9ceb0a40a8a8d6a0185ed3e2
nuruzzaman <mnueuzzaman@gmail.com>
2022-02-17 20:19:44 +0600

    function add

--- app.js
+++ app.js
@@ -1,41 +1,20 @@
-const addPriceBtn = document.getElementById('calBtn');
-addPriceBtn.addEventListener('click', function(){
-    console.log('dd');
-    const inputFoodName = document.getElementById('input-Food').value;
-    const inputRentName = document.getElementById('input-Rent').value;
-    const inputClothesName = document.getElementById('input-Clothes').value;
-
-    // console.log(inputFoodName);
-    // console.log(inputRentName);
-    // console.log(inputClothesName);
-    const totalexpence =  parseInt(inputFoodName)+ parseInt(inputRentName)+ parseInt(inputClothesName);
+function calBtn() {
+    const foodCost = document.getElementById('input-food').value;
+    const rentCost = document.getElementById('input-rent').value;
+    const clothesCost = document.getElementById('input-clothes').value;
+    const totalexpence =  parseInt(foodCost)+ parseInt(rentCost)+ parseInt(clothesCost);
      document.getElementById('total-exp').innerText = totalexpence;
      const income = document.getElementById('income').value;
      const blance =  parseInt(income) - totalexpence;
      
      document.getElementById('blance').innerText = blance;
-
-
-})
-const saveamount = document.getElementById('saveamount');
-saveamount.addEventListener('click', function(){
-    //console.log('dd');
+  };
+function saveamount() {
     const parcentagemany = document.getElementById('parcentagemany').value;
-
-     console.log(parcentagemany);
-    // console.log(inputRentName);
-    // console.log(inputClothesName);
     const blance = document.getElementById('blance').innerText;
-    console.log(blance);
      const saving =parseInt(blance) * parseInt(parcentagemany) /100 ;
-     console.log(saving);
      document.getElementById('saving').innerText = saving;
-
      const remainingb =  blance - saving;
-     
      document.getElementById('remainingb').innerText = remainingb;
-
-
-
-})
+ }
 

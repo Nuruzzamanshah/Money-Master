aef92dfb66dcfdc0607655480e0c6868b37629ec
nuruzzaman <mnueuzzaman@gmail.com>
2022-02-17 16:31:52 +0600

    html add

---
+++ app.js
@@ -0,0 +1,7 @@
+const addPriceBtn = document.getElementById('calBtn');
+addPriceBtn.addEventListener('click', function(){
+    const inputFoodName = document.getElementById('input-Food');
+    const inputRentName = document.getElementById('input-Rent');
+    const inputClothesName = document.getElementById('input-Clothes');
+    console.log('hello mama');
+})

5831d7e4d1efd6fe07bca6fd1c6aa0c6d1284fd2
nuruzzaman <mnueuzzaman@gmail.com>
2022-02-17 20:40:39 +0600

    function added

--- app.js
+++ app.js
@@ -3,18 +3,24 @@ function calBtn() {
     const rentCost = document.getElementById('input-rent').value;
     const clothesCost = document.getElementById('input-clothes').value;
     const totalexpence =  parseInt(foodCost)+ parseInt(rentCost)+ parseInt(clothesCost);
-     document.getElementById('total-exp').innerText = totalexpence;
-     const income = document.getElementById('income').value;
-     const blance =  parseInt(income) - totalexpence;
+    const income = document.getElementById('income').value;
+    
+
+    document.getElementById('total-exp').innerText = totalexpence;
+   
+    const blance =  parseInt(income) - totalexpence;
      
      document.getElementById('blance').innerText = blance;
   };
-function saveamount() {
+function saveAmount() {
     const parcentagemany = document.getElementById('parcentagemany').value;
+    
     const blance = document.getElementById('blance').innerText;
-     const saving =parseInt(blance) * parseInt(parcentagemany) /100 ;
-     document.getElementById('saving').innerText = saving;
-     const remainingb =  blance - saving;
-     document.getElementById('remainingb').innerText = remainingb;
+    const saving =parseInt(blance) * parseInt(parcentagemany) /100 ;
+    document.getElementById('saving').innerText = saving;
+    const remainingb =  blance - saving;
+    document.getElementById('remainingb').innerText = remainingb;
+
+    
  }
 
